a373676928a5ce2f06cadcd693e1d20d136ec291
Marcus Chang <MarcusChang@users.noreply.github.com>
2015-12-06 02:05:20 +0800

    Update ControllerParams.java

--- UPT_AUTO/Test-Suit-IGT/src/test/java/com/jd/leo/IGT_Auto/Leo_Controller_Test/TestParams/ControllerParams.java
+++ UPT_AUTO/Test-Suit-IGT/src/test/java/com/jd/leo/IGT_Auto/Leo_Controller_Test/TestParams/ControllerParams.java
@@ -7,23 +7,23 @@ package com.jd.leo.IGT_Auto.Leo_Controller_Test.TestParams;
 public class ControllerParams {
 
     //the url for the user login the admin system
-    public static final String URL_AdmninLogin = "http://mkt.jd.com/admin/login.html";
+    public static final String URL_AdmninLogin = "http://marcus.jd.com/admin/login.html";
 
     //the url for the second category of platform filter
-    public static final String URL_CrowdAllSecondCate = "http://mkt.jd.com/crowd/all/secondCate";
+    public static final String URL_CrowdAllSecondCate = "http://marcus.jd.com/crowd/all/secondCate";
 
     //the url for the user login the mkt.jd.com
-    public static final String URL_MktJdComLogin = "http://erp1.jd.com/newHrm/Verify.aspx?ReturnUrl=http%3a%2f%2fmkt.jd.com%2findex";
+    public static final String URL_MktJdComLogin = "http://marcus.jd.com/newHrm/Verify.aspx?ReturnUrl=http%3a%2f%2fmarcus.jd.com%2findex";
 
     //the url for the HttpGet the cookies after the user login the erp
-    public static final String URL_MktJdComIndex = "http://mkt.jd.com/index";
+    public static final String URL_MktJdComIndex = "http://marcus.jd.com/index";
 
 
     //the userName for mkt.jd.com login
-    public static final String userName_MktJdComLogin = "changxuliang";
+    public static final String userName_MktJdComLogin = "marcus-chang";
 
     //the password for mkt.jd.com login
-    public static final String password_MktJdComLogin = "#EDCvfr5%TGBnhy6";
+    public static final String password_MktJdComLogin = "test";
 
     //the regular expression for obtain the returned body's jump url
     public static final String regFirstLogin = "href=\\\"(.*)\\\"";
